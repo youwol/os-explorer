953a141c42af5c4ddcdc98d4c25b10f9e199ab07
greinisch <greinisch@youwol.com>
2022-05-26 10:32:48 +0200

    👽️ tests => sync with new http-clients

--- src/tests/common.ts
+++ src/tests/common.ts
@@ -15,9 +15,7 @@ export function resetPyYouwolDbs$() {
             mergeMap(() =>
                 forkJoin([
                     youwol.admin.customCommands.doGet$({ name: 'reset' }),
-                    gtw.explorerDeprecated
-                        .getDefaultUserDrive$()
-                        .pipe(raiseHTTPErrors()),
+                    gtw.explorer.getDefaultUserDrive$().pipe(raiseHTTPErrors()),
                 ]),
             ),
             map(([_, userDrive]) => {
@@ -26,18 +24,21 @@ export function resetPyYouwolDbs$() {
         )
 }
 
-export function createStory(
-    title: string,
-): (src: Observable<unknown>) => Observable<AssetsGateway.Asset> {
+export function createStory(title: string) {
     const client = new AssetsGateway.Client()
     return (source$: Observable<unknown>) => {
         return source$.pipe(
-            mergeMap(() => client.explorerDeprecated.getDefaultUserDrive$()),
+            mergeMap(() => client.explorer.getDefaultUserDrive$()),
             raiseHTTPErrors(),
-            mergeMap((drive: AssetsGateway.DefaultDriveResponse) =>
-                client.assetsDeprecated.story
-                    .create$(drive.homeFolderId, {
-                        title,
+            mergeMap((drive) =>
+                client.stories
+                    .create$({
+                        body: {
+                            title,
+                        },
+                        queryParameters: {
+                            folderId: drive.homeFolderId,
+                        },
                     })
                     .pipe(raiseHTTPErrors()),
             ),
@@ -89,12 +90,4 @@ export function queryFromDocument<T, U = HTMLDivElement>(
     return Array.from(views).filter((v: VDomType<U, T>) =>
         filterFct(v),
     ) as unknown as VDomType<U, T>[]
-}
-
-export function createRemoteFolder$(youwolClient, parentFolderId, name) {
-    return youwolClient.admin.customCommands.doPost$('create-remote-folder', {
-        parentFolderId: parentFolderId,
-        name: name,
-        folderId: name,
-    })
 }
